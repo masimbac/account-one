abcb9b5e98c38d6fbe884a47e96dca4366498d12
Masimba Chingwende <mechingwende@gmail.com>
2021-09-18 17:22:50 +0200

    Added api project

---
+++ account-one-api/src/main/java/za/co/dandemutande/accountoneapi/AccountOneApiApplication.java
@@ -0,0 +1,13 @@
+package za.co.dandemutande.accountoneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountOneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountOneApiApplication.class, args);
+	}
+
+}
---
+++ account-one-api/src/test/java/za/co/dandemutande/accountoneapi/AccountOneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.dandemutande.accountoneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountOneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
